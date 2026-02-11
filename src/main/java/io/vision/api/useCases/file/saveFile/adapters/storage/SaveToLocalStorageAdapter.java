cec2ed1ef269aff16f9c072235e69d1611fba039
youngmok.kim <indiemaru@gmail.com>
2026-02-12 05:11:14 +0900

    ✦ refactor: 예외 처리 로직 개선 및 GlobalWebExceptionHandler에 IllegalArgumentException 핸들러 추가

--- src/main/java/io/vision/api/useCases/file/saveFile/adapters/storage/SaveToLocalStorageAdapter.java
+++ src/main/java/io/vision/api/useCases/file/saveFile/adapters/storage/SaveToLocalStorageAdapter.java
@@ -2,17 +2,18 @@ package io.vision.api.useCases.file.saveFile.adapters.storage;
 
 import io.vision.api.useCases.file.saveFile.application.SaveToFileStorage;
 import io.vision.api.useCases.file.saveFile.application.domain.model.FileStorageType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
@@ -30,17 +31,16 @@ public class SaveToLocalStorageAdapter implements SaveToFileStorage {
 
   @Override
   public void operate(InputStream inputStream, String createSubPath, String createFilename)
-      throws IOException {
+    throws IOException {
 
     Path filePath = Paths.get(localPath, createSubPath);
     Files.createDirectories(filePath);
 
-    Path destinationFile =
-        (filePath.resolve(Paths.get(createFilename)).normalize().toAbsolutePath());
+    Path destinationFile = (filePath.resolve(Paths.get(createFilename)).normalize().toAbsolutePath());
 
     // 상위 디렉토리로 이동하는 경로(Path Traversal) 시도는 차단
     if (!destinationFile.getParent().equals(filePath.toAbsolutePath())) {
-      throw new RuntimeException("Cannot store file outside current directory.");
+      throw new IllegalArgumentException(String.format("[%s] 허용되지 않은 상위 디렉토리 접근 시도가 감지되었습니다.", createFilename));
     }
 
     Files.copy(inputStream, destinationFile, StandardCopyOption.REPLACE_EXISTING);
