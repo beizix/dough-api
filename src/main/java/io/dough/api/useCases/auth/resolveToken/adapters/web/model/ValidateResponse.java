132d993e034570edd960561e61454ad7d298b48c
youngmok.kim <indiemaru@gmail.com>
2026-03-20 09:22:16 +0900

    ✦ refactor: 토큰 확인 유스케이스 어댑터 패키지 이동 및 구조 정돈

--- src/main/java/io/dough/api/useCases/auth/resolveToken/adapters/web/model/ValidateResponse.java
+++
@@ -1,6 +0,0 @@
-package io.dough.api.useCases.auth.resolveToken.adapters.web.model;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(description = "토큰 검증 응답")
-public record ValidateResponse(@Schema(description = "토큰 유효 여부") boolean valid) {}
